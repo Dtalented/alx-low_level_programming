f95f2e7d663abc30022f1ef3a26f033299e36a80
Dtalented <dtalented16@gmail.com>
2022-09-10 03:17:32 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
-‚Å*/
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
