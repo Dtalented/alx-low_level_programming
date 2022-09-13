5e7de73f3ea41f8702f3aeda48fb1a7fd35e5a0e
Dtalented <dtalented16@gmail.com>
2022-09-13 09:41:42 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,22 +1,33 @@
-#include <stdio.h> 
-/** 
- * main - print if the number is postive, zero, or negative 
- * 
- * Description: using the main function 
- * this program prints "Programming is positive, zero, or negative 
- * Return: 0 
- */ 
-int main(void) 
-{ 
-char ch; 
-for (ch = 'a' ; ch <= 'z' ; ch++) 
-{ 
- putchar(ch); 
-} 
-for (ch = 'A' ; ch <= 'Z' ; ch++) 
-{ 
- putchar(ch); 
-} 
-putchar('\n'); 
-return (0);
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  int i, j;
+
+  for (i = 0; i < 100; i++)
+  {
+    for (j = 0; j < 100; j++)
+    {
+      if (i < j)
+      {
+        putchar((i / 10) + 48);
+        putchar((i % 10) + 48);
+        putchar(' ');
+        putchar((j / 10) + 48);
+        putchar((j % 10) + 48);
+        if (i != 98 || j != 99)
+        {
+          putchar(',');
+          putchar(' ');
+        }
+      }
+
+    }
+  }
+  putchar('\n');
+  return (0);
 }
