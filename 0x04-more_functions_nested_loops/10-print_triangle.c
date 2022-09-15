bd6136e4f7f0477d375262853a3a080c2240cb37
Dtalented <dtalented16@gmail.com>
2022-09-15 15:11:56 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
-*main- prints the largest prime factor
-*of a number
-*
-*Return: returns 0
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return: returns nothing
 */
 
-int main(void)
+void print_triangle(int size)
 {
-	long number = 612852475143;
-	int inc;
+	int inc1, inc2;
 
-	while (inc++ < number / 2)
+	if (size > 0)
 	{
-	if (number % inc == 0)
-	{
-		number /= 2;
-		continue;
-	}
+		for (inc1 = 1; inc1 <= size; inc1++)
+		{
+			for ((inc2 = size - inc1); inc2 > 0; inc2--)
+			{
+				putchar(' ');
+			}
 
-	for (inc = 3; inc < number / 2; inc += 2)
-	{
-		if (number % inc == 0)
-			number /= inc;
+			for (inc2 = 0; inc2 < inc1; inc2++)
+			{
+				putchar('#');
+			}
+
+			if (inc1 == size)
+			{
+				continue;
+			}
+
+			putchar('\n');
+		}
 	}
-	}
-	printf("%ld\n", number);
-	return (0);
+	putchar('\n');
 }
