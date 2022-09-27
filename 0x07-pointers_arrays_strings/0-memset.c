4170017b4f99e62eaa951a34aa8641db237a1b67
Dtalented <dtalented16@gmail.com>
2022-09-26 21:15:43 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,24 +1,19 @@
  
- ​#​include​ ​"​main.h​" 
  ​/*​* 
- ​ * _memset - fills memory with a constant byte 
- ​ * @s: pointer block of memory to fill 
- ​ * @b: value to set 
- ​ * @n: bytes of the memory 
- ​ * Return: dest 
+ ​ * _memset - sets a given amount of bytes to a value at an address 
+ ​ * 
+ ​ * @s: memory location to start at 
+ ​ * @b: character to write to memory 
+ ​ * @n: number of bytes to write 
+ ​ * 
+ ​ * Return: pointer to s 
  ​ ​*/ 
-  
  ​char​ *​_memset​(​char​ *s, ​char​ b, ​unsigned​ ​int​ n) 
-  
  ​{ 
- ​        ​unsigned​ ​int​ i; 
+ ​        ​char​ *ptr = s; 
   
- ​        ​/*​Declaring FOR​*/ 
- ​        ​for​ (i = ​0​; i < n; i++) 
- ​        { 
- ​                *(s + i) = b; ​/*​add 1 position s​*/ 
-  
- ​        } ​/*​END FOR​*/ 
+ ​        ​while​ (n-- > ​0​) 
+ ​                *ptr++ = b; 
   
  ​        ​return​ (s); 
  ​}
