75abfe41b21a22587168524b6a255306174d194d
Dtalented <dtalented16@gmail.com>
2022-10-07 23:25:23 -0700

    Trust no one

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,17 +1,21 @@
- 
- ​#​include​ ​"​main.h​" 
-  
- ​/*​* 
- ​ * malloc_checked - allocates memory using malloc 
- ​ * @b: bytes allocate 
- ​ * Return: pointer 
- ​ ​*/ 
- ​void​ *​malloc_checked​(​unsigned​ ​int​ b) 
- ​{ 
- ​        ​void​ *ptr; 
-  
- ​        ptr = ​malloc​(b); 
- ​        ​if​ (ptr == ​NULL​) 
- ​                ​exit​(​98​); 
- ​        ​return​ (ptr); 
- ​}
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+int _putchar(char c);
+
+void *malloc_checked(unsigned int b);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+
+#endif /*MAIN_H*/
