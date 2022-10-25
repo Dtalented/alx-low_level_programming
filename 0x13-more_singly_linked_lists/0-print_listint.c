6ab6fd0edee7b86ee7cd22dff96b6c66090ba7de
Dtalented <dtalented16@gmail.com>
2022-10-24 21:11:25 -0700

    0 print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 
- ​#​include​ ​"​lists.h​"
+​#​include​ ​"​lists.h​"
 
- ​/*​*
- ​ * print_listint - prints all the elements of a list.
- ​ * @h: head of a list.
- ​ *
- ​ * Return: numbers of nodes.
- ​ ​*/
- ​size_t​ ​print_listint​(​const​ ​listint_t​ *h)
- ​{
- ​        ​size_t​ nnodes = ​0​;
+​/*​*
+​ * print_listint - prints all the elements of a list.
+​ * @h: head of a list.
+​ *
+​ * Return: numbers of nodes.
+​ ​ */
+​size_t​ ​print_listint​(​const​ ​listint_t​  * h)
+​{
+​        ​size_t​ nnodes  =  ​0​;
 
- ​        ​while​ (h != ​NULL​)
- ​        {
- ​                ​printf​(​"​%d​\n​"​, h->​n​);
- ​                h = h->​next​;
- ​                nnodes++;
- ​        }
- ​        ​return​ (nnodes);
- ​}
+​        ​while​ (h  !=   ​NULL​)
+​        {
+​           ​printf​(​"​%d​\n​"​,  h->​n​);
+​                h  =  h->​next​;
+​                nnodes++;
+​        }
+​        ​return​ (nnodes);
+​}
